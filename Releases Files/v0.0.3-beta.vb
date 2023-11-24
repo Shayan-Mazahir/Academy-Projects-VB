b585256c68ef525f61757df443174b3fcf114c58
Ruz <shayanmazahir321@gmail.com>
2023-11-24 22:51:01 +0500

    Create v0.0.3-beta.vb

---
+++ Releases Files/v0.0.3-beta.vb
@@ -0,0 +1,17 @@
+Imports System.Console
+
+Module Module1
+' by importing system.console we dont have to write console.writeline again and again
+    Sub Main()
+        WriteLine("Welcome to version v0.0.3-beta")
+        WriteLine("Welcome to programming")
+    End Sub
+End Module
+
+Module Module2
+' by importing system.console we dont have to write console.writeline again and again
+    Sub Main()
+        WriteLine("Welcome to version v0.0.3-beta")
+        WriteLine("Welcome to programming")
+    End Sub
+End Module
