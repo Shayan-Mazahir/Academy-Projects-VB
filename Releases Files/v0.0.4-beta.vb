0a35682ac67f59c42597a34e3608326fd5d4ff78
Ruz <shayanmazahir321@gmail.com>
2023-11-24 22:55:51 +0500

    Create v0.0.4-beta.vb

---
+++ Releases Files/v0.0.4-beta.vb
@@ -0,0 +1,12 @@
+Imports System.Console
+
+Module Module1
+    Sub Main()
+       WriteLine("Welcome to version v0.0.4-beta")
+       Dim n As Integer
+       write("Enter your number: ")
+       n = ReadLine()
+       n = n + 1
+       WriteLine("Addition is " & n)
+    End Sub
+End Module
