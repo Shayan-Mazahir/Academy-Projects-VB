a80ab1264dc3a8626363b2a89d98f57c959b05fa
Ruz <shayanmazahir321@gmail.com>
2023-11-24 22:41:28 +0500

    Create v0.0.1-beta.vb

---
+++ Releases Files/v0.0.1-beta.vb
@@ -0,0 +1,7 @@
+Module Module1
+' by importing system.console we dont have to write console.writeline again and again
+    Sub Main()
+        Console.WriteLine("Welcome to version v0.0.1-beta")
+        Console.WriteLine("Welcome to programming")
+    End Sub
+End Module
